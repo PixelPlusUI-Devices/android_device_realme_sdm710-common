b29676cd4cf9c4b64f25ea5c555e77ba39c238c1
emufan4568 <geoster3d@gmail.com>
2021-12-26 04:20:14 +0000

    sdm710-common: Use prebuilt libutils from R for Oppo health service
    * AOSP 12 removed one of the String8 constructors [1] resulting in this error:
    
    CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.health@2.1-service": cannot locate symbol "_ZN7android7String8C1ENS0_13StaticLinkageE"
    
    To fix it, copy the prebuilt libutils from v30 prebuilts and patch the health service to use it
    
    [1] https://github.com/ArrowOS/android_system_core/commit/e51c026e
    
    Signed-off-by: ad1tyas1ngh
    <adityasingh1768@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -460,6 +460,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
